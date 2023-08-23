1c90c573b2ca1b5d1eb0d3836950080cd001c84f
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2023-08-23 18:54:41 -0400

    rearranged some methods

--- src/Encryption.java
+++ src/Encryption.java
@@ -7,15 +7,14 @@ import java.security.spec.*;
 import java.util.*;
 
 public class Encryption {
-    public static final String DEFAULT_INITIALIZATION_VECTOR = "bOcaFVdcPGgDw3A82k/4bw==";
+    private static String DEFAULT_INITIALIZATION_VECTOR = "bOcaFVdcPGgDw3A82k/4bw==";
+    public static String getDefaultIV() {return DEFAULT_INITIALIZATION_VECTOR;}
+    public static void setDefaultIV(String IV) {DEFAULT_INITIALIZATION_VECTOR = IV;}
 
     // ENCRYPTION
     public static byte[] encrypt(byte[] message, String password, String initializationVector) {
         try {
-            IvParameterSpec IV = new IvParameterSpec(Base64.getDecoder().decode(initializationVector));
-            SecretKeySpec KEY = getKeyFromPassword(password);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.ENCRYPT_MODE, KEY, IV);
+            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, password, initializationVector);
             byte[] encrypted = cipher.doFinal(message);
             return encrypted;
         } catch (Exception e) {
@@ -25,6 +24,7 @@ public class Encryption {
     }
     public static String encrypt(String message, String password, String initializationVector) {
         byte[] out = encrypt(message.getBytes(), password, initializationVector);
+        if (out == null) return null;
         return Base64.getEncoder().encodeToString(out);
     }
     public static String encrypt(String message, String password) {return encrypt(message, password, DEFAULT_INITIALIZATION_VECTOR);}
@@ -32,10 +32,7 @@ public class Encryption {
     // DECRYPTION
     public static byte[] decrypt(byte[] code, String password, String initializationVector) {
         try {
-            IvParameterSpec IV = new IvParameterSpec(Base64.getDecoder().decode(initializationVector));
-            SecretKeySpec KEY = getKeyFromPassword(password);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, KEY, IV);
+            Cipher cipher = getCipher(Cipher.DECRYPT_MODE, password, initializationVector);
             byte[] decrypted = cipher.doFinal(code);
             return decrypted;
         } 
@@ -45,11 +42,22 @@ public class Encryption {
     }
     public static String decrypt(String code, String password, String initializationVector) {
         byte[] out = decrypt(Base64.getDecoder().decode(code), password, initializationVector);
+        if (out == null) return null;
         return new String(out);
     }
     public static String decrypt(String code, String password) {return decrypt(code, password, DEFAULT_INITIALIZATION_VECTOR);}
 
     // OTHER METHODS
+    public static Cipher getCipher(int mode, String password, String initializationVector) {
+        try {
+            IvParameterSpec IV = new IvParameterSpec(Base64.getDecoder().decode(initializationVector));
+            SecretKeySpec KEY = getKeyFromPassword(password);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(mode, KEY, IV);
+            return cipher;
+        } catch (GeneralSecurityException e) {e.printStackTrace();}
+        return null;
+    }
     private static SecretKeySpec getKeyFromPassword(String password) {
         try {
             String salt = "salt";
@@ -72,20 +80,5 @@ public class Encryption {
         String hash = encrypt(fileName, fileName, IV);
         hash = hash.replace("/", ".");
         return "file_" + hash + "_.txt";
-    }
-
-
-
-
-
-    public static Cipher getCipher(int mode, String password, String initializationVector) {
-        try {
-            IvParameterSpec IV = new IvParameterSpec(Base64.getDecoder().decode(initializationVector));
-            SecretKeySpec KEY = getKeyFromPassword(password);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(mode, KEY, IV);
-            return cipher;
-        } catch (GeneralSecurityException e) {e.printStackTrace();}
-        return null;
-    }
+    }    
 }
